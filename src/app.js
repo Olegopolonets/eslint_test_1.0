2071aa4489632b29bb6680fd952af0907a823f39
Olegopolonets <opolonetso@gmail.com>
2021-09-25 20:19:19 +0300

    commit_2.0

--- src/app.js
+++ src/app.js
@@ -2,21 +2,19 @@ const a = 1;
 const esss = 2;
 
 function foo(name) {
-	   const lastName = name;
+	const lastName = name;
 	return lastName;
 }
 
 let x = 200;
 
 x++;
-console.log(x);
+const sum = x - esss;
+console.log('delete: ', sum);
 const e = 123;
-
-const name = obj.name;
-
 console.log(e);
-
 foo('123');
-
 const y = 200;
 console.log(y, a);
+const r = y;
+console.log(r, y, x);
